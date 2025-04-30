05fd3743432005ffeb29f4bf8e9b7b8a70a22c87
Sherif Fouad <sherifdevv@gmail.com>
2025-05-01 00:35:28 +0300

    new updates

--- src/app/ui/map/map.component.ts
+++ src/app/ui/map/map.component.ts
@@ -17,6 +17,27 @@ export class MapComponent implements OnInit, AfterViewInit {
   ngOnInit(): void {}
 
   ngAfterViewInit(): void {
+    // Configure marker icon
+    const iconRetinaUrl =
+      'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-icon-2x.png';
+    const iconUrl =
+      'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-icon.png';
+    const shadowUrl =
+      'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-shadow.png';
+
+    const iconDefault = L.icon({
+      iconRetinaUrl,
+      iconUrl,
+      shadowUrl,
+      iconSize: [25, 41],
+      iconAnchor: [12, 41],
+      popupAnchor: [1, -34],
+      tooltipAnchor: [16, -28],
+      shadowSize: [41, 41],
+    });
+
+    L.Marker.prototype.options.icon = iconDefault;
+
     this.map = L.map('map').setView([this.lat, this.lng], 12);
 
     L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
