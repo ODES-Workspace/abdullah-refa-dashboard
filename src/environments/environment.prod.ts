d8b1b8325410763e7ee0fdf09d288a20c2d3704f
shabbarcendas <44211820+MuhammadShabbarAbbas@users.noreply.github.com>
2025-10-18 15:11:07 +0500

    updated api link to p rod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  baseUrl: 'https://api.refa.sa/api', // Update this to your production API URL
+  baseUrl: 'https://prod.refa.sa/api', // Update this to your production API URL
   // Add other environment-specific configurations here
 };
